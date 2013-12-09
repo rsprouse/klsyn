b591c00efbd374a554a4fcb3edfb643eaf1d3b02
Ronald Sprouse <ronald@berkeley.edu>
2013-12-09 09:39:34 -0800

    Better handling of input arguments.

--- scripts/klattsyn.py
+++ scripts/klattsyn.py
@@ -9,17 +9,25 @@ Created on Wed Nov 20 14:40:35 2013
 # This is an implementation of the Klatt synthesizer suitable for running
 # in batch mode.
 
-import sys
+import os, sys
 import scipy.io.wavfile
 import klsyn.klatt_wrap as klatt_wrap
 from klsyn.klpfile import klpfile
 
+Usage = 'klattsyn paramfile ... [paramfileN]'
+
 if __name__ == '__main__':
     ''' Run the Klatt synthesizer, produce output .wav and write out complete .klp parameter file.'''
-# TODO: check command line arguments
-    synth = klatt_wrap.synthesizer()
-    params = klpfile().read(sys.argv[1] + '.klp')
-    synth.set_params(params)
-    (d,rate) = synth.synthesize()
-    scipy.io.wavfile.write(sys.argv[1] + '.wav', rate, d)
-    klpfile().write(sys.argv[1] + '.wav.klp', synth=synth)
+    try:
+        sys.argv[1] != None
+    except:
+        raise Exception('Usage: {:s}'.format(Usage))
+
+    for pfile in sys.argv[1:]:
+        fname, fext = os.path.splitext(pfile)
+        synth = klatt_wrap.synthesizer()
+        params = klpfile().read(pfile)
+        synth.set_params(params)
+        (d,rate) = synth.synthesize()
+        scipy.io.wavfile.write(fname + '.wav', rate, d)
+        klpfile().write(fname + '.full.klp', synth=synth)
