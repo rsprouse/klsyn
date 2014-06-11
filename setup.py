4d76032152ec9211d13e19f67c85fdc9914340ea
Ronald Sprouse <ronald@berkeley.edu>
2014-06-11 16:00:43 -0700

    Updated documentation.

--- setup.py
+++ setup.py
@@ -5,13 +5,15 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy
 
-ext_modules = [Extension(
+ext_modules = [
+  Extension(
     name="klsyn.klatt_wrap",
     sources=["klsyn/klatt_wrap.pyx"],
     libraries = ["m"],
     include_dirs=[numpy.get_include()],
     language="c",
-    )]
+  )
+]
 
 setup(
   name = 'klsyn',
@@ -19,5 +21,15 @@ setup(
   #ext_package='klatt_wrap',
   ext_modules = ext_modules,
   packages = ['klsyn'],
-  scripts = ['scripts/klattsyn.py', 'scripts/klattsyn_interactive.py', 'scripts/doc2klp.py']
+  scripts = [
+    'scripts/klattsyn.py',
+    'scripts/klattsyn_interactive.py',
+    'scripts/doc2klp.py',
+    'scripts/wxklsyn.pyw'
+  ],
+  classifiers = [
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Multimedia :: Sound/Audio :: Speech'
+  ]
 )
